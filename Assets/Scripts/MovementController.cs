f95a9f39187f4486b17f0d338d3617101ccf75d6
dhdylan <dhdylan@gmail.com>
2020-02-27 14:34:11 -0500

    working on movement system

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -58,6 +58,51 @@ public class MovementController : MonoBehaviour
 
 	#endregion
 
+    public void Move(MovementInput Input)
+    {
+        if (Input.directionalInput.x > 0)
+        {
+            normalizedHorizontalSpeed = 1;
+            if (transform.localScale.x < 0f)
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+            //if( _controller.isGrounded )
+            //_animator.Play( Animator.StringToHash( "Run" ) );
+        }
+        else if (Input.directionalInput.x < 0)
+        {
+            normalizedHorizontalSpeed = -1;
+            if (transform.localScale.x > 0f)
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+            //if( _controller.isGrounded )
+            //_animator.Play( Animator.StringToHash( "Run" ) );
+        }
+        else
+        {
+            normalizedHorizontalSpeed = 0;
+
+            if (_controller.isGrounded)
+                _animator.Play(Animator.StringToHash("Idle"));
+        }
+
+        // we can only jump whilst grounded
+        if (_controller.isGrounded && Input.jump == true)
+        {
+            Debug.Log("jump");
+            _velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+            //_animator.Play( Animator.StringToHash( "Jump" ) );
+        }
+
+        // if holding down bump up our movement amount and turn off one way platform detection for a frame.
+        // this lets us jump down through one way platforms
+        if (_controller.isGrounded && Input.directionalInput.y < 0)
+        {
+            _velocity.y *= 3f;
+            _controller.ignoreOneWayPlatformsThisFrame = true;
+        }
+    }
+
 
 	// the Update loop contains a very simple example of moving the character around and controlling the animation
 	void Update()
@@ -65,55 +110,12 @@ public class MovementController : MonoBehaviour
 		if( _controller.isGrounded )
 			_velocity.y = 0;
 
-		if( Input.GetKey( KeyCode.RightArrow ) )
-		{
-			normalizedHorizontalSpeed = 1;
-			if( transform.localScale.x < 0f )
-				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
-
-			//if( _controller.isGrounded )
-				//_animator.Play( Animator.StringToHash( "Run" ) );
-		}
-		else if( Input.GetKey( KeyCode.LeftArrow ) )
-		{
-			normalizedHorizontalSpeed = -1;
-			if( transform.localScale.x > 0f )
-				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
-
-			//if( _controller.isGrounded )
-				//_animator.Play( Animator.StringToHash( "Run" ) );
-		}
-		else
-		{
-			normalizedHorizontalSpeed = 0;
-
-			if( _controller.isGrounded )
-				_animator.Play( Animator.StringToHash( "Idle" ) );
-		}
-
-
-		// we can only jump whilst grounded
-		if( _controller.isGrounded && Input.GetKeyDown( KeyCode.UpArrow ) )
-		{
-			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
-			//_animator.Play( Animator.StringToHash( "Jump" ) );
-		}
-
-
 		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
 		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
 		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
 
 		// apply gravity before moving
 		_velocity.y += gravity * Time.deltaTime;
-
-		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
-		// this lets us jump down through one way platforms
-		if( _controller.isGrounded && Input.GetKey( KeyCode.DownArrow ) )
-		{
-			_velocity.y *= 3f;
-			_controller.ignoreOneWayPlatformsThisFrame = true;
-		}
 
 		_controller.move( _velocity * Time.deltaTime );
 
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class UserInput : MonoBehaviour
 {
     private MovementController movementController;
+    private MovementInput movementInput;
 
     void Start()
     {
+        movementInput = new MovementInput();
         movementController = GetComponent<MovementController>();
     }
     void Update()
     {
+        movementInput.GetInput();
+        movementController.Move(movementInput);
+        Debug.Log(movementInput.directionalInput + "      &&      " + movementInput.jump);
     }
 }
