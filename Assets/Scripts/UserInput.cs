1f1bc266058188318345fe63542290f117ca43ba
dhdylan <dhdylan@gmail.com>
2020-02-27 13:37:40 -0500

    nevermind, 'it' is gonna be simple. got rid of old movementScripts and reorganized

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -12,10 +12,5 @@ public class UserInput : MonoBehaviour
     }
     void Update()
     {
-        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        if(directionalInput != null)
-        {
-            movementController.Move(directionalInput);
-        }
     }
 }
