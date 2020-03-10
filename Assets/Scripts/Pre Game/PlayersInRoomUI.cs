87db30714e8bff1e880f13980f3c118b912f8846
dhdylan <dhdylan@gmail.com>
2020-03-10 09:47:18 -0400

    I've been working on UI for like 2 days why am I in this branch

---
+++ Assets/Scripts/Pre Game/PlayersInRoomUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class PlayersInRoomUI : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> playerListings = new List<GameObject>();
+    [SerializeField]
+    private GameObject playerListingPrefab;
+    [SerializeField]
+    private RectTransform verticalLayoutGroupTransform;
+
+
+    public void UpdateUI(Dictionary<int, Player> playersInRoom)
+    {
+        clearPlayerListings();
+
+        populatePlayerListings(playersInRoom);
+    }
+
+    private void clearPlayerListings()
+    {
+        foreach (GameObject listing in playerListings)
+        {
+            PhotonNetwork.Destroy(listing);
+        }
+        playerListings.Clear();
+    }
+
+    private void populatePlayerListings(Dictionary<int, Player> playersInRoom)
+    {
+        foreach (Player player in playersInRoom.Values)
+        {
+            GameObject playerListing = Instantiate(playerListingPrefab, verticalLayoutGroupTransform);
+            playerListings.Add(playerListing);
+            playerListing.transform.Find("Username").GetComponent<Text>().text = player.NickName;
+            playerListing.transform.Find("Is Master Client").GetComponent<Text>().text = player.IsMasterClient.ToString();
+            playerListing.transform.Find("Is Local").GetComponent<Text>().text = player.IsLocal.ToString();
+            playerListing.transform.Find("Actor ID").GetComponent<Text>().text = player.ActorNumber.ToString();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Pre Game/PreGameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PreGameManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private PlayersInRoomUI playersInRoomUI;
+
+    void Start()
+    {
+        playersInRoomUI.UpdateUI(PhotonNetwork.CurrentRoom.Players);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        playersInRoomUI.UpdateUI(PhotonNetwork.CurrentRoom.Players);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        playersInRoomUI.UpdateUI(PhotonNetwork.CurrentRoom.Players);
+    }
+}
