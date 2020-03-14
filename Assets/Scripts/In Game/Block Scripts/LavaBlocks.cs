71f74e3a8ac2b2ae6aeb573d0744e372dc950247
dhdylan <dhdylan@gmail.com>
2020-03-14 14:43:19 -0400

    Implemented one of the Tilemap scripts - the lava blocks.
    when aplayer collides with a lava block, it adds an upward force to the player.
    I also added the "IHazardous" interface, but I'm unsure if Im going to use it yet. Kinda seems redundant

---
+++ Assets/Scripts/In Game/Block Scripts/LavaBlocks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBlocks : MonoBehaviour
+{
+    public float upwardForce = 100f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9) // everything inside this statement will happen to the player when it touches lava.
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * upwardForce, ForceMode2D.Impulse);
+        }
+    }
+}
